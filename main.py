c08b4954d39c65bedbe18237d345b342e4c65444
Tamil Arasan <74599391+tamilts124@users.noreply.github.com>
2023-04-24 22:34:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from pyngrok import ngrok
 from os import popen
 
 ngrok.set_auth_token('2OLoyviHU2LQT99KyyAFQQwTLFM_6uEBdpzDg47qZrzqS5JgH')
-tunnel =ngrok.connect(6001, 'tcp').public_url
+tunnel =ngrok.connect('0.0.0.0:5901', 'tcp').public_url
 print(tunnel)
