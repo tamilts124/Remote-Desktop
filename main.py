20ff7b7c0c3b2a31650491157ca8b079436c6f08
Tamil Arasan <74599391+tamilts124@users.noreply.github.com>
2023-04-24 20:35:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from pyngrok import ngrok
+from os import popen
 
 ngrok.set_auth_token('2OLoyviHU2LQT99KyyAFQQwTLFM_6uEBdpzDg47qZrzqS5JgH')
-tunnel =ngrok.connect(5901, 'tcp')
-print(tunnel)
+tunnel =ngrok.connect(5901, 'tcp').public_url
+host, port =tunnel.split(':')[1:]
+host =host[2:]
+out =popen(f'ping -c 1 '+host).read()
+ip =out.split(')')[0].split('(')[-1]
+out =popen(f'vncserver -interface {ip}')
