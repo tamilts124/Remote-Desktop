9e0e9a3a947db70646260e6060aaf3b7e8a119e8
Tamil Arasan <74599391+tamilts124@users.noreply.github.com>
2023-04-24 20:13:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pyngrok import ngrok
 
 ngrok.set_auth_token('2OLoyviHU2LQT99KyyAFQQwTLFM_6uEBdpzDg47qZrzqS5JgH')
-tunnel =ngrok.connect(8080, 'tcp')
+tunnel =ngrok.connect(5901, 'tcp')
 print(tunnel)
