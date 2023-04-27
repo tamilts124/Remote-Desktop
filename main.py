dfce2d6fd6981b7285f939a05384f7f43bd8a5fb
Tamil Arasan <74599391+tamilts124@users.noreply.github.com>
2023-04-27 16:43:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ def main():
     send_Notify(infinitydb, 'Notifier', 'Ubuntu-Remote', 'Info-Normal', message)        
     
     while popen('sudo netstat -tulpn| grep vnc').read():
-        sleep(5)
+        sleep(60)
 
 
 if __name__ == '__main__':
